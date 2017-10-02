6e4057308a8f330a84b9a982c055be5fec2aeb0a
makdenis <makdenis.1997@gmail.com>
2017-10-03 00:32:48 +0300

    added lab3

---
+++ lab3/ex_1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from librip.gen import field, gen_random
+
+goods = [
+    {'title': 'Ковер', 'price': 2000, 'color': 'green'},
+    {'title': 'Диван для отдыха', 'price': 5300, 'color': 'black'}
+]
+
+# Реализация задания 1
+#print([x for x in field(goods, 'title', 'price')])
+#print([x for x in field(goods, 'title')])
+print([x for x in gen_random(1, 5, 5)])
---
+++ lab3/librip/iterators.py
@@ -0,0 +1,46 @@
+import itertools
+
+# Итератор для удаления дубликатов
+class Unique(object):
+    def __init__(self, items, **kwargs):
+        #self.items = items
+        self.items = iter(items) if isinstance(items, list) else items
+        self.ignore_case = False
+        #self.current = 0
+        self.duplicates = []
+        self.ignore_case = kwargs.get('ignore_case', False)
+        #for k, v in kwargs.items():
+        #    if k == 'ignore_case' and v == True:
+        #        self.ignore_case = True
+
+        # Нужно реализовать конструктор
+        # В качестве ключевого аргумента, конструктор должен принимать bool-параметр ignore_case,
+        # в зависимости от значения которого будут считаться одинаковые строки в разном регистре
+        # Например: ignore_case = True, Aбв и АБВ разные строки
+        #           ignore_case = False, Aбв и АБВ одинаковые строки, одна из них удалится
+        # По-умолчанию ignore_case = False
+
+
+    
+
+    def __next__(self):
+        while True:
+            try:
+
+            
+                cur = next(self.items)
+
+                if isinstance(cur, str) and self.ignore_case is True:
+                    cur_check = cur.upper()
+                else:
+                    cur_check = cur
+
+                if not cur_check in self.duplicates:
+                    self.duplicates.append(cur_check)
+                    return cur
+            except Exception:
+                raise StopIteration
+
+
+    def __iter__(self):
+        return self
